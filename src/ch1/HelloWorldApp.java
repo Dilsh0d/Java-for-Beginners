caae480c0e5d77f5d7693c7ba9180d044d392f38
Dilshod Tadjie <d.tadjiev@gmail.com>
2024-09-18 15:22:17 +0500

    1 - Булим Салом Дунё

---
+++ src/ch1/HelloWorldApp.java
@@ -0,0 +1,14 @@
+package ch1;
+/**
+ * HelloWorldApp класс номи
+ *
+ * @author {сизниг исмингиз}
+ */
+public class HelloWorldApp {
+    /**
+     * @param args - бу консолдан бериладиган аргумент узгарувчилари
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");// Экранга печат буладиган кисм
+    }
+}
